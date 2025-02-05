ee61c8802b2e88d1ca5235d4f8e4fa023d3d1f3c
Aekayi Nyo Win <aekari@innovix-solutions.com>
2025-02-05 19:49:02 +0630

    fix issues

--- src/components/Popup/BookingModal.jsx
+++ src/components/Popup/BookingModal.jsx
@@ -92,7 +92,7 @@ const BookingModal = ({ booking, onClose, onEdit, onDelete }) => {
             </div>
           ) : (
             <>
-              {isOwner && booking.status === "pending" && (
+              {isOwner && (
                 <div className="flex justify-end">
                   <button className="px-4 py-2 " onClick={onEdit}>
                     <svg
